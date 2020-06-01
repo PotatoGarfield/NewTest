e732580863bd43d225d0fca4db14e045071750fa
yrn <2401403715@qq.com>
2020-06-02 01:32:21 +0800

    update

--- JavaScript/面向对象.js
+++ JavaScript/面向对象.js
@@ -3,11 +3,13 @@ function book(name, price) {
   this.price = price;
 }
 
+/* 在 prototype 的例子里，printTitle 函数只会创建一次，
+ 在所有实例中共享。如果在类的定义里声明，就像前面的例子一样，
+ 则每个实例都会创建自己的函数副本。 */
 book.prototype.showPrice =  function() {
   i = this.price;
   console.log(this.price);
-  console.log(i,'2');
-  return 'asd';
+  return 'price';
 }
 
 let a = new book('a', 1);
